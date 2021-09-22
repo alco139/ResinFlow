6dfb39eed40aa15060c14343a074d40ea3babb57
alan dolan <alco139@gmail.com>
2021-09-22 13:26:36 +0200

    responzivnost

--- Zadanie3/js/home.js
+++ Zadanie3/js/home.js
@@ -1,6 +1,10 @@
 document.addEventListener( 'DOMContentLoaded', function () {
 	new Splide( '#card-slider', {
-		perPage    : 1,
-		
+		perPage    : 2,
+		breakpoints: {
+			900: {
+				perPage: 1,
+			}
+		}
 	} ).mount();
 } );
